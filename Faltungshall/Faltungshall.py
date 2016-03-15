6b0429ed1c3d8ea0b614000a19aa00af8d3e79db
David Terihaj <david.terihaj@outlook.com>
2016-03-15 19:04:42 +0100

    Added Paths class

--- Faltungshall/Faltungshall.py
+++ Faltungshall/Faltungshall.py
@@ -1,20 +1,45 @@
 from WaveFile import WaveFile
 import threading
 
-ym1path = 'C:\\Users\\terihad\\documents\\visual studio 2015\\Projects\\Faltungshall\\Faltungshall\\Samples\\minster1_000_ortf_48k.wav'
-ym2path = 'C:\\Users\\terihad\\documents\\visual studio 2015\\Projects\\Faltungshall\\Faltungshall\\Samples\\minster1_bformat_48k.wav'
-mh1path = 'C:\\Users\\terihad\\documents\\visual studio 2015\\Projects\\Faltungshall\\Faltungshall\\Samples\\mh3_000_ortf_48k.wav'
-mh2path = 'C:\\Users\\terihad\\documents\\visual studio 2015\\Projects\\Faltungshall\\Faltungshall\\Samples\\mh3_000_bformat_48k.wav'
+class Paths(object):
+    
+    __path = str
+    __ym1path = str
+    __ym2path = str
+    __mh1path = str
+    __mh2path = str
 
-YM1 = WaveFile(ym1path)
-YM2 = WaveFile(ym2path)
-MH1 = WaveFile(mh1path)
-MH2 = WaveFile(mh2path)
+    def __init__(self, path):
+        self.__path = path
+        self.__ym1path = path + 'minster1_000_ortf_48k.wav'
+        self.__ym2path = path + 'minster1_bformat_48k.wav'
+        self.__mh1path = path + 'mh3_000_ortf_48k.wav'
+        self.__mh2path = path + 'mh3_000_bformat_48k.wav'
 
+    def getYM1(self):
+        return self.__ym1path
 
+    def getYM2(self):
+        return self.__ym2path
+
+    def getMH1(self):
+        return self.__mh1path
+
+    def getMH2(self):
+        return self.__mh2path
+
+# Damit man nicht alle Paths für jeden PC neu angeben muss
+paths = Paths('C:\\Users\\david\\Source\\Repos\\Faltungshall\\Faltungshall\\Samples\\')
+
+YM1 = WaveFile(paths.getYM1())
+YM2 = WaveFile(paths.getYM2())
+MH1 = WaveFile(paths.getMH1())
+MH2 = WaveFile(paths.getMH2())
+
+YM1.plot()
 
 #MH1.plot()
-MH2.plotFrequencySpectre()
+#MH2.plotFrequencySpectre()
 
 
 
--- Faltungshall/WaveFile.py
+++ Faltungshall/WaveFile.py
@@ -3,7 +3,6 @@ import numpy as np
 from Helper import *
 from pylab import *
 
-
 class WaveFile(object):
     
     __path = str
